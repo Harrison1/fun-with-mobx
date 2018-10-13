c65d784804beaaaee60e6174434e8581cd9bd7dd
Harrison <hmcguire13@gmail.com>
2018-10-13 13:56:02 -0400

    space cleanup

--- src/stores/Heros.js
+++ src/stores/Heros.js
@@ -1,5 +1,4 @@
 import {observable} from 'mobx';
-import { ALL_TODOS } from '../constants';
 
 export default class ViewStore {
 	@observable heroList = ['Superman', 'Wonder Woman', 'Batman', 'Lara Croft'];
