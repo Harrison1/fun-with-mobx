9c307831f8e8d6b2b22815ab36bd74638b4179ea
Harrison <hmcguire13@gmail.com>
2018-10-20 13:00:43 -0400

    double to single

--- src/components/SecondTodo.js
+++ src/components/SecondTodo.js
@@ -1,7 +1,7 @@
-import React from "react"
-import { observer } from "mobx-react"
-import Todo from "./Todo"
-import TodoListStore from "../stores/TodoList"
+import React from 'react'
+import { observer } from 'mobx-react'
+import Todo from './Todo'
+import TodoListStore from '../stores/TodoList'
 
 const store = new TodoListStore()
 
